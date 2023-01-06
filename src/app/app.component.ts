ce6ef5e090efba225dfdfe1e2525b01587d00d9c
Gabriel <gavelloso@gmail.com>
2023-01-06 15:58:04 -0300

    [SEÇÃO 2] - 18. Converting px to rem: An Effective Workflow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,9 @@
+import { FaviconsService } from './services/favicons.service';
 import { DOCUMENT } from '@angular/common';
-import {
-  AfterViewInit,
-  Component,
-  Inject,
-  OnInit,
-  Renderer2,
-} from '@angular/core';
+import { AfterViewInit, Component, Inject, OnInit, Renderer2 } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { NavigationEnd, Router } from '@angular/router';
 import { filter } from 'rxjs';
-import { FaviconsService } from './services/favicons.service';
 
 @Component({
   selector: 'app-root',
@@ -33,13 +27,13 @@ export class AppComponent {
           this.newTitle = 'Advanced Css Sass'; // Projeto Principal global
           this.renderer2.removeClass(this.document.body, 'NatoursBody');
           this.renderer2.removeClass(this.document.body, 'TrilloBody');
-          this.renderer2.addClass(this.document.body, 'Advanced-css-sass');
+          this.renderer2.addClass(this.document.body, 'AdvancedCssSass');
           this.titleService.setTitle(this.newTitle);
           this.faviconService.updateFavicon('./favicon.ico');
         }
         else if (event.url === '/natours') {
           this.newTitle = 'Natours'; // Projeto 1
-          this.renderer2.removeClass(this.document.body, 'Advanced-css-sass');
+          this.renderer2.removeClass(this.document.body, 'AdvancedCssSass');
           this.renderer2.removeClass(this.document.body, 'TrilloBody');
           this.renderer2.addClass(this.document.body, 'NatoursBody');
           this.titleService.setTitle(this.newTitle);
@@ -47,7 +41,7 @@ export class AppComponent {
         }
         else if (event.url === '/trillo') {
           this.newTitle = 'Trillo'; // Projeto 2
-          this.renderer2.removeClass(this.document.body, 'Advanced-css-sass');
+          this.renderer2.removeClass(this.document.body, 'AdvancedCssSass');
           this.renderer2.removeClass(this.document.body, 'NatoursBody');
           this.renderer2.addClass(this.document.body, 'TrilloBody');
           this.titleService.setTitle(this.newTitle);
